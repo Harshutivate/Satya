b04b1ffbacb6c44d46da1749e7650ddd47cabc92
Harshitha <VHARSHITHA.23@ichat.sp.edu.sg>
2024-05-16 12:16:19 +0800

    bmi python cod

---
+++ Bmi.py
@@ -0,0 +1,24 @@
+'''height = float(input("Enter height: "))
+weight = float(input("Enter weight: "))
+
+BMI = weight/(height*height)
+
+print("This is the calculated Bmi value:",BMI,type(BMI))'''
+
+
+def get_inputs():
+    height = float(input("Enter height: "))
+    weight = float(input("Enter weight: "))
+    return height,weight
+
+def calc(height,weight):
+    BMI = weight/(height*height)
+    return BMI 
+
+def main():
+    height,weight = get_inputs()
+    BMI = calc(height,weight)
+    print("This is the calculated Bmi value:",BMI,type(BMI))
+
+if __name__ == "__main__":
+  main()
